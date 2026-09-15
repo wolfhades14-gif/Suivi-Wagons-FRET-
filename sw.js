8732b428d3e39bf584300c128f3a794ecdab3eec
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-15 23:58:36 +0100

    V11.6 - simplification

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'suivi-wagons-fret-v11-4';
+const CACHE_NAME = 'suivi-wagons-fret-v11-6';
 const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
