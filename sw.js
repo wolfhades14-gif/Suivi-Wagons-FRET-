880c74ceba743176d5d5a3ab9b989cf23dedece4
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-16 23:21:11 +0100

    V12.4- corrections final

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
-const CACHE_NAME = 'suivi-wagons-fret-v12-0';
-const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./header-train-bg.jpg'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', event => { if(event.request.method!=='GET') return; event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html')))); });
+const CACHE_NAME = 'suivi-wagons-fret-v12-4';
+const APP_SHELL = [
+  './',
+  './index.html?v=12.4',
+  './manifest.json',
+  './header-train-bg.jpg',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html?v=12.4'))));
+});
