e5f58264938402ee31112dbab2573de9fcd391e5
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-17 13:41:47 +0100

    V12.6 - corrections cashe

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'suivi-wagons-fret-v12-5';
+const CACHE_NAME = 'suivi-wagons-fret-v12-6';
 const APP_SHELL = [
   './',
-  './index.html?v=12.4',
+  './index.html',
   './manifest.json',
   './header-train-bg.jpg',
   './icon-192.png',
@@ -9,16 +9,28 @@ const APP_SHELL = [
   './icon-512-maskable.png'
 ];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
-    return response;
-  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html?v=12.4'))));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+  );
 });
