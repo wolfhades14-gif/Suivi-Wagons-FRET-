4fc75425a9d96aa45c833c9ea97152f9f2d8e56e
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-18 23:48:18 +0100

    V13.3-export mobile corrigé

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "suivi-wagons-fret-v13-2";
-const ASSETS = ["./","./index.html?v=13.1","./manifest.json","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./header-train-bg.jpg"];
+const CACHE_NAME = "suivi-wagons-fret-v13-3";
+const ASSETS = ["./","./index.html?v=13.3","./manifest.json","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./header-train-bg.jpg"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
