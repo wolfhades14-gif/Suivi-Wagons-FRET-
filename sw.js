d380bead27690188f703b1ee65a45a3bf3d469a8
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-18 19:31:05 +0100

    V13.0- pilotage sans doublons

--- sw.js
+++ sw.js
@@ -1,36 +1,5 @@
-const CACHE_NAME = 'suivi-wagons-fret-v12-7';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './header-train-bg.jpg',
-  './icon-192.png',
-  './icon-512.png',
-  './icon-512-maskable.png'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
-  );
-});
+const CACHE_NAME = "suivi-wagons-fret-v13-0";
+const ASSETS = ["./","./index.html?v=13.0","./manifest.json","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./header-train-bg.jpg"];
+self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
