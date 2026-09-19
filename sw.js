5d0286bd005ee5937315b380d97fdfa24bde35bb
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-19 10:26:53 +0100

    V17-Export complet, motifs multiples

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "suivi-wagons-fret-v16-0";
+const CACHE_NAME = "suivi-wagons-fret-v17-0";
 const ASSETS = ["./","./index.html?v=15.0","./manifest.json","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./header-train-bg.jpg"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
