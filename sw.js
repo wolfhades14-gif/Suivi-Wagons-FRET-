f502ca62798c2a573e84b448facbe868e6946c67
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-20 13:56:44 +0100

    V19 - corrections complète et simplifié

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "suivi-wagons-fret-v18-0";
-const ASSETS = ["./","./index.html?v=18.0","./manifest.json","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./header-train-bg.jpg"];
-self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
-self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))); });
+const CACHE_NAME = 'suivi-wagons-fret-v19-0';
+const ASSETS = ['./','./index.html','./manifest.json','./header-train-bg.jpg','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', event => { if(event.request.method!=='GET') return; event.respondWith(fetch(event.request).then(response=>{const copy=response.clone(); caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)); return response;}).catch(()=>caches.match(event.request))); });
