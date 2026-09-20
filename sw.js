5b99752e3046bf1446804461b16460df09b864e8
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-20 14:26:23 +0100

    V21- étiquette automatique

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'suivi-wagons-fret-v20-0';
+const CACHE_NAME = 'suivi-wagons-fret-v21-0';
 const ASSETS = ['./','./index.html','./manifest.json','./header-train-bg.jpg','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
