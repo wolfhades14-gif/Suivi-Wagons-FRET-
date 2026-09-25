36968c07479b59639820c7f27d379343a2f49b4e
wolfhades14-gif <wolfhades14@gmail.com>
2026-09-25 13:53:42 +0100

    Update Supabase publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@
 // NE METS JAMAIS une sb_secret_... ou service_role ici.
 window.SUPABASE_CONFIG = {
   url: "https://kmeekaefnunccnzwalac.supabase.co",
-  publishableKey: "REMPLACER_PAR_TA_SB_PUBLISHABLE_KEY",
+  publishableKey: "sb_publishable_uQ5MQJbEf5JzbLyLVpU_5A_5VJ9kx8D",
   controllerEmails: []
 };
